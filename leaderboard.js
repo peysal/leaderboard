d83763af2cde5b8a294b85d9d4a6987a52731162
peysal <peysal@gmail.com>
2014-03-03 17:16:25 +0800

    sorting

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,13 @@ Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
   Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
+    var sortRules;
+    if(Session.get("sort_direction") === "name") {
+      sortRules = {score: -1, name: 1};
+    } else {
+      sortRules = {score: 1, name: -1}
+    }
+    return Players.find({}, {sort: sortRules});
   };
 
   Template.leaderboard.selected_name = function () {
@@ -16,6 +22,23 @@ if (Meteor.isClient) {
   Template.player.selected = function () {
     return Session.equals("selected_player", this._id) ? "selected" : '';
   };
+  
+  Session.set("sort_direction", "name");  //initialize 1 time
+  Template.leaderboard.sort_direction = function() {
+    return Session.get("sort_direction");     
+  };
+
+  Template.leaderboard.events({
+    'click input.sort': function () {
+      //console.log("before " + Session.get("sort_direction"));
+      if(Session.get("sort_direction") === "name") {
+        Session.set("sort_direction", "point");  
+      } else {
+        Session.set("sort_direction", "name");  
+      }
+      //console.log("after " + Session.get("sort_direction"));
+    }
+  });  
 
   Template.leaderboard.events({
     'click input.inc': function () {
@@ -33,7 +56,7 @@ if (Meteor.isClient) {
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Players.find().count() === 0) {
+    if (Players.find().count() === 0) { //i need to clean up db if want to see the changes
       var names = ["Ada Lovelace",
                    "Grace Hopper",
                    "Marie Curie",
@@ -41,7 +64,7 @@ if (Meteor.isServer) {
                    "Nikola Tesla",
                    "Claude Shannon"];
       for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+        Players.insert({name: names[i], score: Math.floor(Random.fraction()*100)*5});
     }
   });
 }
